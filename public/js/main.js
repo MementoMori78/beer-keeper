ede40fb46f0cf4f339245ffe54b645bae577f0f8
Vladyslav Tertyshnyi <vlad.tertyshny@gmail.com>
2019-05-29 17:27:15 +0300

    Initial commit

---
+++ public/js/main.js
@@ -0,0 +1,54 @@
+$(function () {
+    if ($('textarea#ta').length) {
+        CKEDITOR.replace('ta');
+    }
+    $('a.confirmDeletion').on('click', function () {
+        if (!confirm('Confirm deletion'))
+            return false;
+    });
+
+    if ($("[data-fancybox]").length) {
+        $("[data-fancybox]").fancybox();
+    }
+});
+
+
+$(document).ready(function () {
+    $('#search').keyup(function () {
+        search_table($(this).val());
+    });
+    function search_table(value) {
+        $('#products_table tr').each(function () {
+            var found = 'false';
+            $(this).each(function () {
+                if ($(this).text().toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+                    found = 'true';
+                }
+            });
+            if (found == 'true') {
+                $(this).show();
+            }
+            else {
+                $(this).hide();
+            }
+        });
+    }
+    $('#search').focus();
+});
+
+$('#exampleModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('.modal-title').text('Вкажіть кількість для ' + recipient);
+})
+
+$('#exampleModal').on('shown.bs.modal', function () {
+    
+    if($('#quantity').val()) {
+        $('#quantity').val('');
+    }
+    $('#quantity').focus();
+})  
